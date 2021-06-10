ca3272140f305624d193ae844408e3ede7dccdaa
leanneminnock <lelannem91@gmail.com>
2021-06-10 16:15:30 +0100

    module 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import "rxjs/add/operator/map";
+import { Album } from "./album";
+import { Observable  } from "rxjs/Observable";
+
 
 @Injectable()
 export class ProductService {
@@ -9,7 +12,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  public getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response: Response) => response.json() );
+  public  getAlbum(id: number): Observable<Album>{
+    return this._http.get(this._albumUrl).map(response => response.json() as Album);
   }
 }
