aa33d49f1c159429f8dc6d7c64f01518cb6b36fc
leanneminnock <lelannem91@gmail.com>
2021-06-10 16:28:48 +0100

    -changed how response.json() is cast to type album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,6 +13,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   public  getAlbum(id: number): Observable<Album>{
-    return this._http.get(this._albumUrl).map(response => response.json() as Album);
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
